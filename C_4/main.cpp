a9d9b9b7bc14d79847548dffab867eab075cef0a
llirik9 <llirikmedmetskii@gmail.com>
2023-07-28 09:08:00 +0300

    update C_4

--- C_4/main.cpp
+++ C_4/main.cpp
@@ -102,7 +102,7 @@ int main() {
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (j < i) {
+            if (j > i) {
                 x += my_abs(a[i][j]);
             }
         }
